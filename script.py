f2f162c48e146a14e8406fd8ce559fb99194dac6
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-08-25 16:41:15 -0300

    Preparação para início do projeto

--- script.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-if __name__ == '__main__':
-    with open('./world_imdb_movies_top_movies_per_year.csv', newline='', mode='r', encoding="utf8") as database:
-        spamreader = csv.reader(database, delimiter=',')
-
-        for row in spamreader:
-            print(', '.join(row))
